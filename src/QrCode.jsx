e814955d12b5cdff9dd7ee068a23d57fef7d2563
EmmanuelOchaje <114095241+EmmanuelOchaje@users.noreply.github.com>
2025-10-31 11:42:29 +0100

    i got this modified

--- src/QrCode.jsx
+++ src/QrCode.jsx
@@ -1,37 +1,31 @@
 import { useState } from "react";
 import QRCode from "react-qr-code";
+import qrImg from "../src/images/image.png";
 
 const QrCode = () => {
   const [qrcode, setQrCode] = useState("");
   const [input, setInput] = useState("");
 
-  function handleQrcode() {
-    setQrCode(input);
-    setInput("");
-  }
-
   return (
-    <div>
-      <h1 className="text-center font-bold text-3xl my-5">QR Code Generator</h1>
-      <div className="flex justify-center ">
-        <input
-          type="text"
-          name=""
-          className="p-1 border rounded-xl my-2"
-          onChange={(e) => setInput(e.target.value)}
-          placeholder="Input text here"
-          value={input}
-        />
-        <button
-          disabled={input && input.trim() !== "" ? false : true}
-          onClick={handleQrcode}
-          className="bg-black text-white p-1 m-2 rounded-xl"
+    <div className="flex justify-center items-center bg-[#D5E1EF] h-screen">
+      <div className="flex flex-col w-[300px] items-center bg-white p-4 rounded-lg">
+        <a
+          href="http://frontendmentor.io"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          Generate
-        </button>
-      </div>
-      <div className="flex justify-center ">
-        <QRCode value={qrcode} size={300} bgColor="#fff" />
+          <div className="bg-[#2C7DF] rounded-lg w-fit">
+            <img src={qrImg} alt="" className="rounded-lg" />
+            {/* <QRCode value={qrcode} size={200} bgColor="#fff" /> */}
+          </div>
+        </a>
+        <h2 className="text-black/80 text-xl my-3 text-balance text-center font-bold">
+          Improve your front-end skills by building projects
+        </h2>
+        <p className="text-sm text-gray-500 text-center">
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
       </div>
     </div>
   );
